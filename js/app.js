bba2a4f1718d0ca04a88f8e0a5ae1687d4074724
kongyu <kongyu@kongyu.com>
2018-03-26 14:23:51 +0800

    清空了页面,更换了路由

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
-var app =angular.module('QueryApp',['ngRoute', 'ngCookies']);
-    app.config(function($routeProvider){
-        $routeProvider
-        .when('/',{
-        	templateUrl:"pages/query.html",
+var app =angular.module('QueryApp',['ui.router', 'ngCookies']);
+    app.config(function($stateProvider,$urlRouterProvider){
+    	$stateProvider
+        .state("1",{
+        	url:"/",
+        	templateUrl:"pages/1.html",
         	controller:"QueryController"
         })
-        .when('/dataBase',{
-        	templateUrl:"pages/dataBase.html",
-        	controller:"DataBaseController"
+        .state("2",{
+        	url:"/2",
+        	templateUrl:"pages/2.html",
+        	controller:"QueryController"
         })
-        .when('/dashboard',{
-        	templateUrl:"pages/dashboard.html",
-        	controller:"DashboardController"
+        .state("3",{
+        	url:"/3",
+        	templateUrl:"pages/3.html",
+        	controller:"QueryController"
         })
-         .when('/day',{
-        	templateUrl:"pages/day.html",
-        	controller:"DayController"
-        })
-        .otherwise({redirectTo:'/'});
+        .state("4",{
+        	url:"/4",
+        	templateUrl:"pages/3.html",
+        	controller:"QueryController"
+        });
+        $urlRouterProvider.otherwise("/")
+//      $routeProvider
+//      .when('/',{
+//      	templateUrl:"pages/query.html",
+//      	controller:"QueryController"
+//      })
+//      .when('/dataBase',{
+//      	templateUrl:"pages/dataBase.html",
+//      	controller:"DataBaseController"
+//      })
+//      .when('/dashboard',{
+//      	templateUrl:"pages/dashboard.html",
+//      	controller:"DashboardController"
+//      })
+//       .when('/day',{
+//      	templateUrl:"pages/day.html",
+//      	controller:"DayController"
+//      })
+//      .otherwise({redirectTo:'/'});
     });
